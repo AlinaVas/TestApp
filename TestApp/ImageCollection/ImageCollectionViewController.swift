700602f646479ad8ce2c475594516e8760d152b9
Alina Fesyk <fesyk.alina@gmail.com>
2019-04-01 00:06:59 +0300

    ImageCollectionView UI set

---
+++ TestApp/ImageCollection/ImageCollectionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ImageCollectionViewController.swift
+//  TestApp
+//
+//  Created by Alina Fesyk on 4/1/19.
+//  Copyright © 2019 Alina FESYK. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ TestApp/ImageCollection/ImageViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageViewCell.swift
+//  TestApp
+//
+//  Created by Alina Fesyk on 4/1/19.
+//  Copyright © 2019 Alina FESYK. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewCell: UICollectionViewCell {
+    
+    
+}
